45443be3835696925751a881cfaaa2e3cc7ac93b
mcalex468 <alexmaco468@gmail.com>
2025-02-04 20:00:31 +0100

    commit exercici 1/2/3

--- pinia-course/src/App.vue
+++ pinia-course/src/App.vue
@@ -1,35 +1,41 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import { useProductStore } from "@/stores/ProductStore";
-import { useCartStore } from "@/stores/CartStore";
-const productStore = useProductStore();
-const cartStore = useCartStore();
+import { useProductStore } from "./stores/useProductStore";
+import { useCartStore } from "./stores/useCartStore";
 
-const addToCart = (count, product) => {
+const productStore=useProductStore();
+const cartStore=useCartStore();
+productStore.fill()
 
-  count = parseInt(count)
-  cartStore.$patch(state => {
-    for (let index = 0; index < count; index++) {
-      state.items.push(product)
+// Elminem la funció del patch , per probar un altre 
+// Patch ens mostra els canvis OldValue NewValue
+/*const addToCart=(count,product)=>{
+  count=parseInt(count)
+  cartStore.$patch(state=>{
+    for(let index=0;index<count;index++){
+    state.items.push(product);      
     }
   })
-}
-productStore.fill();
+}*/
+
 </script>
 
 <template>
   <div class="container">
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
-      <!--ProductCard
+       <ProductCard
         v-for="product in productStore.products"
         :key="product.name"
         :product="product"
         @addToCart="cartStore.addItems($event, product)"
-      /-->
-      <ProductCard v-for="product in productStore.products" :key="product.name" :product="product"
-        @addToCart="addToCart($event, product)" />
+      />
+      <!--<ProductCard v-for="product in productStore.products"
+        :key="product.name" :product="product"
+        @addToCart="addToCart($event,product)"
+      />-->
     </ul>
   </div>
 </template>
+
--- pinia-course/src/stores/useCartStore.js
+++ pinia-course/src/stores/useCartStore.js
@@ -1,4 +1,7 @@
 import { defineStore } from "pinia";
+import { groupBy } from "lodash";
+import { useAuthUserStore } from "./AuthUserStore";
+
 export const useCartStore = defineStore("CartStore", {
   state: () => {
     return {
@@ -12,5 +15,51 @@ export const useCartStore = defineStore("CartStore", {
         this.items.push({ ...item });
       }
     },
+    setItemCount(item, count) {
+      // Si la quantitat és 0 o negativa, eliminem el producte del carret
+      if (count <= 0) {
+        this.clearItem(item.name);
+        return;
+      }
+      // Busquem l'índex del producte dins del carret
+      const index = this.items.findIndex((i) => i.name === item.name);
+      // Si el producte existeix, actualitzem la quantitat
+      if (index !== -1) {
+        // Substituïm el producte existent per un nou amb la quantitat actualitzada
+        this.items.splice(index, 1, { ...item, quantity: count });
+      }
+    },
+    checkout() {
+      const authUserStore = useAuthUserStore();
+      alert(`${authUserStore.username} name just bought ${this.count} items at a total of $${this.total}`)
+    },
+
+
   },
+  getters: {
+    /*count(){
+      return this.items.length
+    },
+    isEmpty(){
+      return this.count===0;
+    }*/
+    // Getter per comptar el nombre total d'elements a la llista
+    count: (state) => state.items.length,
+
+    // Getter per comprovar si la llista està buida
+    isEmpty: (state) => state.count === 0,
+
+    // Agrupa els elements per nom
+    grouped: (state) => groupBy(state.items, (item) => item.name),
+
+    // Retorna el nombre d'elements en un grup específic
+    groupCount: (state) => (name) => state.grouped[name].length,
+
+    // Getter per calcular el preu total de tots els productes
+    totalPrice: (state) => {
+      return state.items.reduce((total, item) => total + item.price, 0)
+      // Suma cada preu al total acumulat 0); 
+      // Valor inicial de l'acumulador és 0
+    }
+  }
 });
