d81c53c7d91db12065c8ca32db636867c59244dd
AlexMartin <alex.martin.7e7@itb.cat>
2025-02-11 12:34:51 +0100

    commit social media

--- pinia-course/src/components/TheHeader.vue
+++ pinia-course/src/components/TheHeader.vue
@@ -1,14 +1,20 @@
 <script setup>
-// imports
-import CartWidget from './CartWidget.vue';
+import { storeToRefs } from "pinia";
+import { useAuthUserStore } from "@/stores/AuthUserStore";
+import CartWidget from "@/components/CartWidget.vue";
+
+const authUserStore = useAuthUserStore();
+const { username } = storeToRefs(authUserStore);
+const { visitProfile } = authUserStore;
 </script>
 
 <template>
-  <header 
-    class="flex justify-between p-6 mb-10 items-center"
-    style="background-image: url('/images/double-bubble-outline.png')"
-  >
+  <header class="flex justify-between p-6 mb-10 items-center"
+    style="background-image: url('/images/double-bubble-outline.png')">
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
-    <CartWidget/>
+    <div>
+      <span class="mr-5 cursor-pointer" title="SocialMedia" @click="visitProfile">{{ username }}</span>
+      <CartWidget class="inline-block" />
+    </div>
   </header>
 </template>
--- pinia-course/src/stores/AuthUserStore.js
+++ pinia-course/src/stores/AuthUserStore.js
@@ -1,9 +1,18 @@
-import {defineStore} from "pinia"
+import { defineStore, acceptHMRUpdate } from "pinia";
 export const useAuthUserStore=defineStore("AuthUserStore",{
    state:()=>{
 //Aquí ho faríem amb un action, però per resumir ho fem hardcoded
        return {
-           username:"Afegeix el teu nom",
+           username:"Alex_Martin",
        }
-   }
-})
+   },
+   actions: {
+    visitProfile() {
+      window.open(`https://instagram.com/${this.username}`, "_blank");
+    },
+  },
+});
+
+if (import.meta.hot) {
+    import.meta.hot.accept(acceptHMRUpdate(useAuthUserStore, import.meta.hot));
+  }
